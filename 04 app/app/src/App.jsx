f4711c6c201084beb5c9cf3b2e355c436fe2e0fe
nilubhai93 <niladrimaji9382@gmail.com>
2025-07-28 15:13:45 +0530

    basic of react

---
+++ 04 app/app/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css"
+
+function Component(){
+    return(
+        <div>
+            <h1> Hello </h1>
+            <p> lorem hvgfv dhfu ui ui </p>
+        </div>
+    )
+}
+
+function App(){
+    return(
+        <div>
+            <Component/>
+            <Component/>
+        </div>
+    )
+}
+// export default Component
+export default App
